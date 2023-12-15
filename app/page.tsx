9c8bf5f10abf177bdde1f73cdacd2df8f63e2ee5
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-14 14:40:54 -1000

    initial commit, design skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,111 @@
 import Image from 'next/image'
+import Link from "next/link"
+import { Badge } from "@/components/ui/badge"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { SelectValue, SelectTrigger, SelectItem, SelectContent, Select } from "@/components/ui/select"
+import { TableCell, TableRow, TableBody, Table } from "@/components/ui/table"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="max-w-4xl mx-auto p-6 bg-white">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-xl font-bold">Emergent Ventures Winners</h1>
+        <div className="flex space-x-4">
+          <Link className="text-blue-600" href="#">
+            about
+          </Link>
+          <Link className="text-blue-600" href="#">
+            github
+          </Link>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="bg-[#00c79f] p-4 rounded-lg mb-6 text-black">
+        <h2 className="font-semibold mb-2">Semantic search over every Emergent Ventures winner</h2>
+        <p className="mb-4 text-sm">
+          This site uses semantic similarity to search all Emergent Ventures grantees. This search bar doesn't need you to get the keywords exactly right, only close enough, to find them.
+        </p>
+        <p className="mb-4 text-sm">
+          You can search for something very specific like "Ukraine" or "career development", or something very broad like
+          "books" or "podcasts". Here are a few starting places:
+        </p>
+        <div className="flex flex-wrap gap-2 mb-4">
+          <Badge variant="secondary">Progress studies</Badge>
+          <Badge variant="secondary">Climate change</Badge>
+          <Badge variant="secondary">AI safety</Badge>
+          <Badge variant="secondary">Career development</Badge>
+          <Badge variant="secondary">Podcasts</Badge>
+          <Badge variant="secondary">Blogs and Substacks</Badge>
+          <Badge variant="secondary">Biotech</Badge>
+          <Badge variant="secondary">Nuclear</Badge>
+        </div>
+        <div className="flex">
+          <Input className="flex-1 mr-2" placeholder="Name a company or pitch an idea..." />
+          <Button>Search</Button>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex items-center mb-4">
+        <Select className="w-16">
+          <SelectTrigger id="batch">
+            <SelectValue placeholder="Select a cohort..." />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            <SelectItem value="w09">Cohort 1</SelectItem>
+            <SelectItem value="s09">Cohort 2</SelectItem>
+            <SelectItem value="w10">Cohort 3</SelectItem>
+          </SelectContent>
+        </Select>
       </div>
-    </main>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">Airbnb</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Book accommodations around the world.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Volantio</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Post-booking revenue and capacity optimization for airlines...</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Wattvision</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Sensors and software for energy tracking. Like 'fitbit' for your...</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Cloudkick</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Cloudkick is system to manage cloud servers, such as the Rackspace Cloud and Ama...</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Echodio</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Sync and stream your music across any device. Acquired in 2009.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">reMail</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Email search for iPhone. Acquired by Google in Feb 2010.</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Divvyshot</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Divvyshot is a photo-sharing platform that enables users to share full resolutio...</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Heyzap</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Heyzap (acquired by Fyber for $45m in 2016) is one of the largest...</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-medium">Voxli</TableCell>
+            <TableCell>W09</TableCell>
+            <TableCell>Voxli offers browser-based group voice chat for gamers and teams.</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+
   )
 }
