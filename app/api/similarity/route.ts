b73ee42b75d577e72894e9ce93c2237e8d6d8ee2
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-16 11:56:45 -1000

    fix get_sentiment function naming

--- app/api/similarity/route.ts
+++ app/api/similarity/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { pipeline } from '@xenova/transformers';
 
-async function get_sentiment(query: string) {
+export async function getSentiment(query: string) {
     let extractor = await pipeline('feature-extraction', 'Xenova/all-mpnet-base-v2');
     let output = await extractor(query, { pooling: 'mean', normalize: true });
 
@@ -11,6 +11,6 @@ async function get_sentiment(query: string) {
 export async function GET(request: NextRequest) {
     const { searchParams } = new URL(request.url)
     const query = searchParams.get('query') || ''
-    const sentiment = await get_sentiment(query)
+    const sentiment = await getSentiment(query)
     return NextResponse.json({ query: query, message: sentiment }, { status: 200 });
 }
