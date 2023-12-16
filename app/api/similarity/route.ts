6888a0eab27975430ee667a7d188607dc927fa90
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2023-12-16 08:28:53 -1000

    fix linting errors

--- app/api/similarity/route.ts
+++ app/api/similarity/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { pipeline } from '@xenova/transformers';
 
-async function get_sentiment(query) {
+async function get_sentiment(query: string) {
     let extractor = await pipeline('feature-extraction', 'Xenova/all-mpnet-base-v2');
     let output = await extractor(query, { pooling: 'mean', normalize: true });
 
