1a2e39cd1a29f7bc0ed9e63c115fc9b6aa22f7e1
Nabeel Qureshi <nabeelqureshi67@gmail.com>
2024-12-18 13:17:59 +0000

    cohort 39

--- app/footer.tsx
+++ app/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
             <p className="mb-4">Written in <a className="underline" href="https://nextjs.org/">next.js</a>, with semantic search thanks to <a className="underline" href="https://huggingface.co/docs/transformers.js/index">transformers.js</a> and <a className="underline" href="https://www.sbert.net/">sentence-transformers</a>. The model is <a href="https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2" className="underline">all-MiniLM-L6-v2</a>.</p>
             <p className="mb-4">Data sourced by Nabeel from <a className="underline" href="https://marginalrevolution.com">Marginal Revolution</a> and starting from <a className="underline" href="https://newscience.org/emergent-ventures-winners/">Alexey Guzey&lsquo;s base.</a></p>
             <p className="mb-4"><a className="underline" href="https://www.mercatus.org/emergent-ventures">Emergent Ventures</a> lives at the Mercatus Center and was founded by <a className="underline" href="https://en.wikipedia.org/wiki/Tyler_Cowen">Tyler Cowen</a>. This is not an official website.</p>
-            <p className="mb-4">Last updated December 2024, up to cohort #38. Submit a data update to the <a className="underline" href="https://github.com/nqureshi/ev-search-python/tree/main/data">Github repo</a>.</p>
+            <p className="mb-4">Last updated December 2024, up to cohort #39. Submit a data update to the <a className="underline" href="https://github.com/nqureshi/ev-search-python/tree/main/data">Github repo</a>.</p>
         </>
     )
 }
